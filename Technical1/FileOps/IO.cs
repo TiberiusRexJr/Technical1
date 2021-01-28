d27affa48a3eda8e67e1f3136582fe093fdee59b
Lexus <rounin_lopez@yahoo.com>
2021-01-28 10:35:52 -0600

    Models(BillHeader.cs),FileOps(Io.cs,Parsing.cs)

---
+++ Technical1/FileOps/IO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOps.IO
+{
+    class IO
+    {
+        #region Constants
+
+        static string FileNameRPT = "BillFile";
+        #endregion
+
+        #region Constructors
+        public IO() { };
+        #endregion
+
+        #region ReadFile
+        public void ReadXML()
+        {
+
+
+        }
+        public void ReadRPT()
+        {
+
+
+        }
+
+        #endregion
+
+        #region WriteFile
+
+        public void WriteRPT()
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ Technical1/FileOps/Parsing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technical1.FileOps
+{
+    class Parsing
+    {
+        #region Constants
+        static Dictionary<string, string> _FieldTable = new Dictionary<string, string>();
+        
+
+        #endregion
+
+        #region Constructors
+            public Parsing()
+            {
+            _FieldTable.Add("2", "8203ACC7-2094-43CC-8F7A-B8F19AA9BDA2");
+            _FieldTable.Add("5",)
+            }
+        #endregion
+
+        #region Parse
+        public void ParseXML()
+        {
+
+        }
+
+        #endregion
+    }
+}
