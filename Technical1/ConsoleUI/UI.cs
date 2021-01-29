868681b665c92760cf50965e61e17c7a221f1ded
Lexus <rounin_lopez@yahoo.com>
2021-01-28 20:55:35 -0600

    fixed color issue with console ui

--- Technical1/ConsoleUI/UI.cs
+++ Technical1/ConsoleUI/UI.cs
@@ -4,14 +4,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Technical1.Main;
 
 namespace Technical1.ConsoleUI
 {
     
     class UI
     {
-        
+        public void MainLoop()
+        {
+            UI ui = new UI();
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("1. Parse a .XML File to .RPT File");
+            Console.WriteLine("2. Save the contents of a .RPT File to the Database");
+            Console.WriteLine("3. Export the Contents of the Databse to a CSV File");
+            Console.WriteLine("4. Quit The Program");
+
+            Console.WriteLine(Environment.NewLine);
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("Enter a Number Now..");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            string input = Console.ReadLine();
+            int choice = default;
+
+            if (!Int32.TryParse(input, out choice))
+            {
+                Console.WriteLine(Environment.NewLine);
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Make a Valid Selection Dude");
+                Console.WriteLine(Environment.NewLine);
+                Console.ForegroundColor = ConsoleColor.White;
+
+
+                MainLoop();
+            }
+            // Use a switch statement to do the math.
+            switch (choice)
+            {
+                case 1:
+                    ui.XML_To_RPT();
+                    break;
+                case 2:
+                    ui.XML_To_DB();
+                    break;
+                case 3:
+                    ui.CSV_From_DB();
+                    break;
+                case 4:
+                    Environment.Exit(1);
+                    break;
+                default:
+                    MainLoop();
+                    break;
+            }
+            // Wait for the user to respond before closing.
+
+        }
+
         public void XML_To_RPT()
         {
             OpenFileDialog fdb = new OpenFileDialog();
@@ -40,8 +92,9 @@ namespace Technical1.ConsoleUI
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(StatusMessage);
             Console.WriteLine(Environment.NewLine);
+            Console.ResetColor();
 
-
+            MainLoop();
 
         }
 
@@ -69,7 +122,9 @@ namespace Technical1.ConsoleUI
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(StatusMessage);
             Console.WriteLine(Environment.NewLine);
+            Console.ResetColor();
 
+            MainLoop();
         }
 
         public void CSV_From_DB()
@@ -95,7 +150,10 @@ namespace Technical1.ConsoleUI
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(StatusMessage);
             Console.WriteLine(Environment.NewLine);
+            Console.ResetColor();
 
+
+            MainLoop();
         }
     }
 }
